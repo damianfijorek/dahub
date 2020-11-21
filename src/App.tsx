84b099ff86e4849ebf544b5e462921489fe87b1d
Fijorek <Damian.Fijorek@dnvgl.com>
2020-11-21 10:18:35 +0100

    Add antd component library.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { List } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <h1>GitHub users</h1>
+      <List/>
     </div>
   );
 }
